518dac7592b9aab88b564f2d768d437ed7bac943
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2022-05-11 15:58:55 +0900

    cmake/YmUtils.cmake: 変更
    * ym_use_gtest() を macro から function に変更
    * それに伴って変数の set も修正 (PARENT_SCOPEが必要)
    * オプション引数の解析バグを修正
    * ninja が libgtest.a の依存関係を見つけてくれないバグ対応

--- cmake/YmUtils.cmake
+++ cmake/YmUtils.cmake
@@ -228,27 +228,29 @@ endmacro ()
 #       GTEST_LIBRARY        gtest ライブラリ
 #       GTEST_MAIN_LIBRARY   gtest_main ライブラリ
 #       GTEST_BOTH_LIBRARIES gtest 用のライブラリ
-macro ( ym_use_gtest )
+function ( ym_use_gtest )
   include(ExternalProject)
   find_package(Threads REQUIRED)
 
   set( __git_repository https://github.com/google/googletest.git )
   set( __git_tag release-1.11.0 )
-  foreach ( pos RANGE 0 ${ARGC} )
-    if ( ${pos} EQUAL ${ARGC} )
-      break()
-    endif ()
-    list ( GET ARGV ${pos} __arg )
-    if ( ${__arg} STREQUAL GIT_REPOSITORY )
-      math ( EXPR pos1 "${pos} + 1")
-      list ( GET ARGV ${pos1} __val )
-      set ( __git_repository ${__val} )
-    elseif ( ${__arg} STREQUAL GIT_TAG )
-      math ( EXPR pos1 "${pos} + 1")
-      list ( GET ARGV ${pos1} __val )
-      set ( __git_tag ${__val} )
-    endif ()
-  endforeach ()
+  if ( ${ARGC} )
+    math( EXPR __n "${ARGC} - 1" )
+    foreach ( pos RANGE 0 ${__n} )
+      list ( GET ARGV ${pos} __arg )
+      if ( ${__arg} STREQUAL GIT_REPOSITORY )
+	math ( EXPR pos1 "${pos} + 1")
+	list ( GET ARGV ${pos1} __val )
+	set ( __git_repository ${__val} )
+	set ( pos ${pos1} )
+      elseif ( ${__arg} STREQUAL GIT_TAG )
+	math ( EXPR pos1 "${pos} + 1")
+	list ( GET ARGV ${pos1} __val )
+	set ( __git_tag ${__val} )
+	set ( pos ${pos1} )
+      endif ()
+    endforeach ()
+  endif ()
 
   ExternalProject_Add(
     googletest
@@ -258,15 +260,25 @@ macro ( ym_use_gtest )
     INSTALL_COMMAND ""
     LOG_DOWNLOAD ON
     LOG_CONFIGURE ON
-    LOG_BUILD ON)
+    LOG_BUILD ON
+    # ninja の dependency バグを回避するためのコード
+    # はっきりいって美しくない
+    BUILD_BYPRODUCTS
+    ${CMAKE_CURRENT_BINARY_DIR}/googletest-prefix/src/googletest-build/lib/libgtest.a
+    ${CMAKE_CURRENT_BINARY_DIR}/googletest-prefix/src/googletest-build/lib/libgtest_main.a
+    ${CMAKE_CURRENT_BINARY_DIR}/googletest-prefix/src/googletest-build/lib/libgmock.a
+    ${CMAKE_CURRENT_BINARY_DIR}/googletest-prefix/src/googletest-build/lib/libgmock_main.a
+    )
 
   ExternalProject_Get_Property(googletest source_dir)
-  set(GTEST_INCLUDE_DIRS ${source_dir}/googletest/include)
-  set(GMOCK_INCLUDE_DIRS ${source_dir}/googlemock/include)
+  set(GTEST_INCLUDE_DIRS ${source_dir}/googletest/include PARENT_SCOPE)
+  set(GMOCK_INCLUDE_DIRS ${source_dir}/googlemock/include PARENT_SCOPE)
 
   ExternalProject_Get_Property(googletest binary_dir)
   set(GTEST_LIBRARY_PATH ${binary_dir}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}gtest.a)
+  set(GTEST_LIBRARY_PATH ${GTEST_LIBRARY_PATH} PARENT_SCOPE)
   set(GTEST_LIBRARY gtest)
+  set(GTEST_LIBRARY ${GTEST_LIBRARY} PARENT_SCOPE)
   add_library(${GTEST_LIBRARY} UNKNOWN IMPORTED)
   set_target_properties(${GTEST_LIBRARY} PROPERTIES
     IMPORTED_LOCATION ${GTEST_LIBRARY_PATH}
@@ -274,17 +286,21 @@ macro ( ym_use_gtest )
   add_dependencies(${GTEST_LIBRARY} googletest)
 
   set(GTEST_MAIN_LIBRARY_PATH ${binary_dir}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}gtest_main.a)
+  set(GTEST_MAIN_LIBRARY_PATH ${GTEST_MAIN_LIBRARY_PATH} PARENT_SCOPE)
   set(GTEST_MAIN_LIBRARY gtest_main)
+  set(GTEST_MAIN_LIBRARY ${GTEST_MAIN_LIBRARY} PARENT_SCOPE)
   add_library(${GTEST_MAIN_LIBRARY} UNKNOWN IMPORTED)
   set_target_properties(${GTEST_MAIN_LIBRARY} PROPERTIES
     IMPORTED_LOCATION ${GTEST_MAIN_LIBRARY_PATH}
     IMPORTED_LINK_INTERFACE_LIBRARIES ${CMAKE_THREAD_LIBS_INIT})
   add_dependencies(${GTEST_MAIN_LIBRARY} googletest)
 
-  set(GTEST_BOTH_LIBRARIES ${GTEST_LIBRARY} ${GTEST_MAIN_LIBRARY})
+  set(GTEST_BOTH_LIBRARIES ${GTEST_LIBRARY} ${GTEST_MAIN_LIBRARY} PARENT_SCOPE)
 
   set(GMOCK_LIBRARY_PATH ${binary_dir}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}gmock.a)
+  set(GMOCK_LIBRARY_PATH ${GMOCK_LIBRARY_PATH} PARENT_SCOPE)
   set(GMOCK_LIBRARY gmock)
+  set(GMOCK_LIBRARY ${GMOCK_LIBRARY} PARENT_SCOPE)
   add_library(${GMOCK_LIBRARY} UNKNOWN IMPORTED)
   set_target_properties(${GMOCK_LIBRARY} PROPERTIES
     IMPORTED_LOCATION ${GMOCK_LIBRARY_PATH}
@@ -292,14 +308,16 @@ macro ( ym_use_gtest )
   add_dependencies(${GMOCK_LIBRARY} googletest)
 
   set(GMOCK_MAIN_LIBRARY_PATH ${binary_dir}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}gmock_main.a)
+  set(GMOCK_MAIN_LIBRARY_PATH ${GMOCK_MAIN_LIBRARY} PARENT_SCOPE)
   set(GMOCK_MAIN_LIBRARY gmock_main)
+  set(GMOCK_MAIN_LIBRARY ${GMOCK_MAIN_LIBRARY} PARENT_SCOPE)
   add_library(${GMOCK_MAIN_LIBRARY} UNKNOWN IMPORTED)
   set_target_properties(${GMOCK_MAIN_LIBRARY} PROPERTIES
     IMPORTED_LOCATION ${GMOCK_MAIN_LIBRARY_PATH}
     IMPORTED_LINK_INTERFACE_LIBRARIES ${CMAKE_THREAD_LIBS_INIT})
   add_dependencies(${GMOCK_MAIN_LIBRARY} ${GTEST_LIBRARY})
 
-endmacro ()
+endfunction ()
 
 # ym_init_submodules: サブモジュール関係の変数を設定するマクロ
 #
